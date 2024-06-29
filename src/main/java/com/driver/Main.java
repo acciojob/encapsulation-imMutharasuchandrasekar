43f352adf328860316998efd81916bfd278dfca6
imMutharasuchandrasekar <mutharasuc.prof@gmail.com>
2024-06-29 12:49:30 +0530

    complete encapsulation assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[]args)
+    {
+        RWOnly obj = new RWOnly();
+        obj.setName("Private_name");
+
+        System.out.println(obj.getName());
+    }
 }
